b4259d192bddbd84edbe44f3969d95d46d8b7909
Tejo Nutalapati <tnutala@gmail.com>
2019-06-29 15:56:17 -0500

    first commit

---
+++ GAN_ex.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 17:21:44 2019
+
+@author: tnuta
+"""
+
+
+#However, their is still a gap between the simulated and real 
+#datasets which Deep Convolutional Neural Networks cannot overcome on their own.
+
+
+
+
+
+
